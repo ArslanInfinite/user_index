e620c7a00b61fcb617812b85fa8152a3322564af
Arslan <ali.arslan319@gmail.com>
2022-01-01 23:15:58 -0500

    create AddUser form with Username and Age, and button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,11 @@
 export const AddUser = props => {
-
+  return (
+    <form>
+      <label htmlFor='username'>Username</label>
+      <input id='username' type='text'></input>
+      <label htmlFor='age'>Age</label>
+      <input id='age' type='number'></input>
+      <button type='submit'>Add User</button>
+    </form>
+  )
 }
