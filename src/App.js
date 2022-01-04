e03e34e645b6f43c4a465fc1cbc402e1acbb660a
Arslan <ali.arslan319@gmail.com>
2022-01-03 23:53:28 -0500

    set state in App.js for UsersList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import AddUser from "./components/Users/AddUser"
 import UsersList from "./components/Users/UsersList"
 
 const App = () => {
+
+  const [usersList, setUsersList] = useState([])
+
   return (
     <div>
       <AddUser />
-      <UsersList users={[]}/>
+      <UsersList users={usersList}/>
     </div>
   )
 }
