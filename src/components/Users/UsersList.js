c08e904be33abff55b84b439339cd1cf3cfb9fba
Arslan <ali.arslan319@gmail.com>
2022-01-03 23:39:57 -0500

    list users with name and age

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -3,7 +3,11 @@
 const UsersList = props => {
   return (
     <div>
-      
+      {props.users.map(user => 
+      <li>
+        {user.name} ({user.age} years old)
+      </li>
+      )}
     </div>
   )
 }
